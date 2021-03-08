8d542dd91619dfeff3b29bbcad136fc4fca8f3e4
Kyel John David <kyeljohnmdavid@gmail.com>
2021-03-08 10:55:17 +0800

    made consumer and producer into one

--- eda/src/main/java/com/kyeljmd/eda/service/KafkaMessageProducer.java
+++ eda/src/main/java/com/kyeljmd/eda/service/KafkaMessageProducer.java
@@ -3,7 +3,6 @@ package com.kyeljmd.eda.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kyeljmd.eda.event.messages.pageevent.PageViewDTO;
 import com.kyeljmd.eda.event.messages.pageevent.RegisterApplicantDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -15,27 +14,15 @@ public class KafkaMessageProducer {
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 
-    private static final String topicName = "pageviews-json";
+    private static final String topicName = "PH_SUB_TOPIC";
 
     private static final String new_applicant_topic  = "new-applicants";
 
     private static final ObjectMapper mapper = new ObjectMapper();
 
     public void sendMessage(String currtime) {
-        PageViewDTO pageViewDTO = new PageViewDTO();
-        pageViewDTO.setPage("1");
-        pageViewDTO.setPageCount(23);
-        String pageViewJSON = null;
-
-        try {
-            pageViewJSON = mapper.writeValueAsString(pageViewDTO);
-            System.out.println("Converted POJO to JSON");
-            System.out.println(pageViewJSON);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Sending messagge: "+pageViewJSON);
-        kafkaTemplate.send(topicName, pageViewDTO);
+        System.out.println("Sending messagge: "+currtime);
+        kafkaTemplate.send(topicName, currtime);
     }
 
 
